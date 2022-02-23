34024d144e221a09cd7541338e18547171f38235
Shaevitzaiden <Aidenashaevitz@gmail.com>
2022-02-23 13:28:17 -0800

    added gripper primitives

--- ArmPi/Functions/Motion.py
+++ ArmPi/Functions/Motion.py
@@ -13,12 +13,29 @@ from CameraCalibration.CalibrationConfig import *
 
 
 class MotionPrimitives:
-    def __init__(self) -> None:
+    def __init__(self, arm) -> None:
         self.is_moving = False
         self.action_finished = False
         self.unreachable = False
         self.goal = ()
+        self.servo1 = 500
         
-    def 
+    def open_claw(self):
+        Board.setBusServoPulse(1, self.servo1 - 280, 500)  # paws open
+        time.sleep(1)
+
+    def close_claw(self):
+        Board.setBusServoPulse(1, self.servo1 - 200, 500)  # Claws open to drop objects
+        time.sleep(1)
+
+    def rotate_gripper(self, angle):
+        Board.setBusServoPulse(2, angle, 500)
 
 
+if __name__ == "__main__":
+    arm = ArmIK()
+    mp = MotionPrimitives(arm)
+
+    mp.open_claw()
+    mp.close_claw()
+    mp.rotate_gripper(90)
