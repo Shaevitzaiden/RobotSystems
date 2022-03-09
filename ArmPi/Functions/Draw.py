184b0042a230906c6e0f3ec138c9b2838593ecb8
Shaevitzaiden <Aidenashaevitz@gmail.com>
2022-03-09 12:29:55 -0800

    drawing function added

---
+++ ArmPi/Functions/Draw.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# coding=utf8
+import numpy as np
+import sys
+# import pandas as pd
+sys.path.append('/home/aiden/RobotSystems/ArmPi/')
+import time
+from math import sqrt
+from LABConfig import *
+from ArmIK.Transform import *
+from ArmIK.ArmMoveIK import *
+import HiwonderSDK.Board as Board
+from CameraCalibration.CalibrationConfig import *
+from Motion import Motion
+from DoodleInput import DoodleInput
+
+
+
+if __name__ == "__main__":
+    arm = Motion()
+    doodle = DoodleInput()
+    
+    # coords = pd.read_csv('draw_coords.csv')
+    # coords = coords.to_numpy()
+    coords = np.loadtxt('draw_coords.csv', delimiter=",")
+    coords = coords/10
+    coords = np.round(coords)
+    
+    coords[:,0] -= 10
+    coords[:,1] += 1
+    
+    i = 1
+    while i < len(coords):
+        if (coords[i] == coords[i-1]).all():
+            coords = np.delete(coords, i-1, 0)
+        else:
+            i += 1
+    
+    print("Starting Masta' Peace! (˘ ³˘)♥ ")
+    
+    arm.move_arm(coords[0,0],coords[0,1],10,0,-180,180)
+    time.sleep(0.5)
+    
+    for i in range(len(coords)):
+        print(f"current coordinates: {coords[i]}")
+        arm.move_arm(coords[i,0],coords[i,1],10,0,-180,180)
+        time.sleep(0.1)
+        
+    print("All done!")
