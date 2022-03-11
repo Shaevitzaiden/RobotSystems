c3890b9c80ba2895315ee94c7067fd458463445f
Shaevitzaiden <aidenashaevitz@gmail.com>
2022-03-12 05:16:50 +0800

    discontinuous working

--- ArmPi/Functions/Draw.py
+++ ArmPi/Functions/Draw.py
@@ -22,12 +22,12 @@ class DoodleDraw(Motion):
         self.coords = coords_array
         self._preproces_coords()
         
-        self.move_arm(self.coords[0,0],self.coords[0,1],3,-90,-90,0)
-        time.sleep(0.5)
+        self.move_arm(self.coords[0,0],self.coords[0,1],4,-90,-90,0)
+        time.sleep(1)
         
         for i in range(len(self.coords)-1):
             self.move_arm(self.coords[i,0], self.coords[i,1],-1,-90,-90,0)
-            
+            time.sleep(0.1)
             # Check if next point is discontinuous and if so lift up and go to next point
             coord_dist = sqrt((self.coords[i+1,0]-self.coords[i,0])**2 + (self.coords[i+1,1]-self.coords[i,1])**2)
             if coord_dist > 0.5:
@@ -36,18 +36,19 @@ class DoodleDraw(Motion):
 
     def _reposition(self, current_point, next_point):
         # lift arm
-        self.move_arm(current_point[0], current_point[1], 3, -90, -90, 0)
+        self.move_arm(current_point[0], current_point[1], 4, -90, -90, 0)
 
         # move arm to hover above next point
-        self.move_arm(next_point[0], next_point[1], 3, -90, -90, 0)
-
+        self.move_arm(next_point[0], next_point[1], 4, -90, -90, 0)
+        time.sleep(1)
+        
     def _preproces_coords(self):
         # Center drawing 
         self.coords[:,0] = self.coords[:,0] - (np.amax(self.coords[:,0]) + np.amin(self.coords[:,0]))/2
         self.coords[:,1] = self.coords[:,1] - np.amin(self.coords[:,1])
         # scale drawing to drawable region
         self.coords = self.coords / 10 # naive but seems to work fine
-        self.coords[:,1] += 5
+        self.coords[:,1] += 7.5
         # Round and remove duplicates to speed up drawing
         self.coords = np.round(self.coords, 2)
         i = 1
